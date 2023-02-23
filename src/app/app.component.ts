fda2d1245ad600814e84b3e61d1a89608c2fe77f
Quentin Ordener <quentin.ordener@web-atrio.com>
2023-02-23 15:00:21 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
+import { Card, CARD_LIST, CARD_TYPE } from './card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-}
+export class AppComponent {}
